e7886bf0e802fd32bcd0b854045a3925312d96c9
mooon3356 <mooon3356@gmail.com>
2024-02-25 23:27:20 +0900

    feat: add core component

---
+++ src/core/Component.ts
@@ -0,0 +1,39 @@
+interface Component<S> {
+  setup: () => void;
+  template: () => string;
+  setEvent: () => void;
+  setState: (newState: S) => void;
+  mounted: () => void;
+}
+
+export class BaseComponent<T extends HTMLElement, S = void> implements Component<S> {
+  $target: T;
+  state: S;
+
+  constructor(element: T) {
+    this.$target = element;
+    this.setup();
+    // this.render();
+    // this.setEvent();
+  }
+
+  setup() { }
+
+  template() {
+    return ''
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.mounted();
+  }
+
+  setEvent() { }
+
+  setState(newState: S) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+
+  mounted() { }
+}

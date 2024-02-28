0e93407a031d35ab6ea15dc86375671d00b9d4c7
mooon3356 <mooon3356@gmail.com>
2024-02-28 16:07:25 +0900

    feat(component): add virtual dom and diff algorithm

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -30,7 +30,17 @@ export abstract class BaseComponent<T extends HTMLElement, S = {}, P = {}> imple
   }
 
   render() {
-    this.$target.innerHTML = this.template();
+    const newNode = document.importNode(this.$target, true);
+    newNode.innerHTML = this.template();
+
+    const oldChildNodes = [...this.$target.childNodes];
+    const newChildNodes = [...newNode.childNodes];
+    const max = Math.max(oldChildNodes.length, newChildNodes.length);
+
+    for (let i = 0; i < max; i++) {
+      updateElement(this.$target, newChildNodes[i], oldChildNodes[i]);
+    }
+
     this.mounted();
   }
 
@@ -43,4 +53,42 @@ export abstract class BaseComponent<T extends HTMLElement, S = {}, P = {}> imple
   }
 
   mounted() { }
+}
+// TODO: Refactoring needed
+// @ts-ignore
+function updateElement(parent, newNode, oldNode) {
+  if (!newNode && oldNode) return oldNode.remove();
+  if (newNode && !oldNode) return parent.appendChild(newNode);
+  if (newNode instanceof Text && oldNode instanceof Text) {
+    if (oldNode.nodeValue === newNode.nodeValue) return;
+    oldNode.nodeValue = newNode.nodeValue
+    return;
+  }
+  if (newNode.nodeName !== oldNode.nodeName) {
+    const index = [...parent.childNodes].indexOf(oldNode);
+    oldNode.remove();
+    parent.appendChild(newNode, index);
+    return;
+  }
+  updateAttributes(oldNode, newNode);
+
+  const newChildren = [...newNode.childNodes];
+  const oldChildren = [...oldNode.childNodes];
+  const maxLength = Math.max(newChildren.length, oldChildren.length);
+  for (let i = 0; i < maxLength; i++) {
+    updateElement(oldNode, newChildren[i], oldChildren[i]);
+  }
+}
+
+// @ts-ignore
+function updateAttributes(oldNode, newNode) {
+  for (const { name, value } of [...newNode.attributes]) {
+    console.log(name, value);
+    if (value === oldNode.getAttribute(name)) continue;
+    oldNode.setAttribute(name, value);
+  }
+  for (const { name } of [...oldNode.attributes]) {
+    if (newNode.getAttribute(name) !== undefined) continue;
+    oldNode.removeAttribute(name);
+  }
 }
